ce77fa05b1db6952fa0939e82c5622259ef71d59
dinixweb <sd4live@gmail.com>
2022-08-15 02:36:09 +0100

    initial commit

---
+++ src/test/java/io/dinixweb/SpringbootgraphQL/SpringbootGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dinixweb.SpringbootgraphQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
